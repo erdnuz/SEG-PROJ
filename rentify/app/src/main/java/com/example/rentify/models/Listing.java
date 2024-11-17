1ed5371d0b3a58c4d55c28ff5458ab6caebb1038
Arden McArthur <arden.mcarthur@gmail.com>
2024-11-17 14:00:24 -0500

    Deliverable 3

--- rentify/app/src/main/java/com/example/rentify/models/Listing.java
+++
@@ -1,7 +0,0 @@
-package com.example.rentify.models;
-import java.util.ArrayList;
-
-public class Listing {
-	protected static ArrayList<String> categories = new ArrayList<>();
-	//{"Tools","Camping","Instruments"}
-}
